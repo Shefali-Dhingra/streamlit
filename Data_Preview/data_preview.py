4ead43d89d1d0438f7bf4f9ac42e43b95b0f7223
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 02:59:31 +0530

    Update data_preview.py

--- Data_Preview/data_preview.py
+++ Data_Preview/data_preview.py
@@ -1,7 +1,7 @@
 # General import section
 import streamlit as st #streamlit backend
 
-def main(data_obj):
+def main(df):
     """Data Preview main
 
     :param data_obj: DataObject instance
@@ -9,21 +9,20 @@ def main(data_obj):
     """
     st.header("DATA PREVIEW")
     col1, col2 = st.columns(2)
-    col3 = st.columns(2)
+    col3 = st.columns(1)
     
     with col1:
         st.subheader("Original dataframe")
-        st.dataframe(data_obj.df)
-        st.write(data_obj.df.shape)
+        st.dataframe(df)
+        st.write(df.shape)
         
     with col2:
         st.subheader("Dataframe description")
-        st.dataframe(data_obj.df.describe())
+        st.dataframe(df.describe())
     
     with col3:
         st.subheader("Data types")
-        st.dataframe(data_obj.df.dtypes.astype(str))
-        
+        st.dataframe(df.dtypes.astype(str))
 
 # Main
 if __name__ == "__main__":
