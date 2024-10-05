9a8bc151799e277050f9d517cd55e836835fb588
Shefali Dhingra <71131216+Shefali-Dhingra@users.noreply.github.com>
2024-10-06 02:47:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,58 +19,14 @@ import Data_Preview
 # import Regression
 # import Classification
 
-# Data object class
-class DataObject():
-    """
-    Data object class holds a dataframe and its byte size.
-    """
-    def __init__(self, df=None, filesize=None):
-      """The constructor for DataObject class
-
-      :param df: pandas dataframe object, defaults to None
-      :type df: pandas.core.frame.DataFrame, optional
-      :param filesize: byte size of pandas dataframe, defaults to None
-      :type filesize: numpy.int32, optional
-      """
-      self.df = df
-      self.filesize = filesize
-
-
-# Interface class        
-class Interface():
-    """
-    Interface class contains a file picker and a side bar. It also handles the import of a data object.
-    """
-    def __init__(self):
-      """The constructor for Interface class.
-      """
-      pass
-    
-    def side_bar(cls, dt_obj):
-      """Sidebar configuration and file picker
-
-      :param dt_obj: pandas dataframe object
-      :type dt_obj: pandas.core.frame.DataFrame
-      """
-      # Accepts .csv and .data
-      filename = st.sidebar.file_uploader("Upload a data file", type=(["csv", "data"]))                   
-      if filename is not None: #file uploader selected a file      
-        try: #most datasets can be read using standard 'read_csv'                                                                                           
-            dt_obj.df = pd.read_csv(filename, sep=';|,', decimal=',', engine='python')                                                                     
-            dt_obj.filesize = dt_obj.df.size
-        except: #due to a different encoding some datafiles require additional processing
-            filename.seek(0)
-            filename = filename.read()
-            filename = str(filename,'utf-8')
-            filename = StringIO(filename)
-            #now the standard 'read_csv' should work
-            dt_obj.df = pd.read_csv(filename, sep=';', decimal=',', index_col = False)
+dt_obj.df = pd.read_csv('data/us-population-2010-2019-reshaped.csv')
             ## Add 3001 sample
-            dt_obj.filesize = dt_obj.df.size
-      
-        # Side bar navigation menu with a select box
-        menu = ['Data Preview', 'Data Preparation']
-        navigation = st.sidebar.selectbox(label="Select menu", options=menu)
+dt_obj.filesize = dt_obj.df.size
+
+with st.sidebar:
+  st.title('💻Dashboard💻') 
+  menu = ['Data Preview', 'Data Preparation']
+  navigation = st.sidebar.selectbox(label="Select menu", options=menu)
 
         # Runs 'Data Preview' app
         if navigation == 'Data_Preview':
